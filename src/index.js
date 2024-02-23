29980d63891f6ff12dcb32b1c7080ea2db881819
Shukri Hussein <husseinshukri@outlook.com>
2024-02-23 11:46:02 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+
+  
+// });
+
+
+
+//document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('create-task-form');
+  const taskInput = document.getElementById('new-task-description');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const taskText = taskInput.value;
+    
+    if (taskText.trim() !== '') {
+      const newTask = document.createElement('li');
+      newTask.textContent = taskText;
+      taskList.appendChild(newTask);
+      
+      taskInput.value = ''; // Clear input field after adding task
+    }
+  });
